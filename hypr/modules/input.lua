ace443ba4b7bff66b661df778b9e96f6c8899b77
Oliver Saal <oliver.saal@proton.me>
2026-06-22 21:48:56 +0300

    migrate hyprland: input and gestures; remove unnecessary example device

---
+++ hypr/modules/input.lua
@@ -0,0 +1,24 @@
+-- Based on ad1822/hyprdots@e7d1663
+
+hl.config({
+    input = {
+        kb_layout = "fi",
+        follow_mouse = 1,
+        sensitivity = 0.0,
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
+
+-- Three-fingered L/R swipe: change workspace
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace",
+})
+
+hl.device({
+    name = "epic-mouse-v1",
+    sensitivity = -0.5
+})
