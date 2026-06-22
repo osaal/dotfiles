cb452ef4d11039218bef4de0322a0e0976eea470
Oliver Saal <oliver.saal@proton.me>
2026-06-22 21:07:20 +0300

    migrate hyprland: monitors, variables

---
+++ hypr/modules/monitor.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+    output = "eDP-1",
+    mode = "2560x1440",
+    position = "0x0",
+    scale = 1.333,
+})
+
+hl.monitor({
+    output = "DP-1",
+    mode = "1920x1080",
+    position = "-1920x0",
+    scale = 1,
+})
+
+hl.monitor({
+    output = "HDMI-A-1",
+    mode = "1920x1080",
+    position = "-1920x0",
+    scale = 1,
+})
---
+++ hypr/modules/variables.lua
@@ -0,0 +1,12 @@
+# Based on ad1822/hyprdots@e7d1663
+
+local variables = {
+    terminal = "kitty",
+    fileManager = "yazi",
+    menu = "/home/osaal/.config/rofi/scripts/launcher_t5",
+    browser = "firefox",
+    taskManager = "btop",
+    note = "obsidian",
+}
+
+return variables
