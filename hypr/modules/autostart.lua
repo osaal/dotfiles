24dbfecb709126671677cf1c6bdc6a36bfb7c534
Oliver Saal <oliver.saal@proton.me>
2026-07-15 20:42:09 +0300

    change autostarting programs, document some hyprlock todos

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -7,8 +7,9 @@ local function exec_once(cmd)
 end
 
 hl.on("hyprland.start", function()
+    exec_once("swaybg -i /home/osaal/dotfiles/wallpapers/bg_2.jpg -m stretch")
     exec_once("waybar")
+
     exec_once("dunst &")
     exec_once("systemctl --user start hyprpolkitagent")
-    exec_once("swaybg -i /home/osaal/dotfiles/wallpapers/bg_2.jpg -m stretch")
 end)
