dff2d54e00f644d6f64a2baa7f468ea9445bedbf
Oliver Saal <oliver.saal@proton.me>
2026-07-15 20:48:19 +0300

    change general settings

--- hypr/modules/general.lua
+++ hypr/modules/general.lua
@@ -2,23 +2,42 @@
 
 hl.config({
     general = {
-        gaps_in = 5,
-        gaps_out = 20,
-        border_size = 4,
+        gaps_in = 3,
+        gaps_out = 6,
+        border_size = 1,
         col = {
-            active_border = "rgba(99,9,9,1.0)",
-            inactive_border = "rgba(595959aa)",
+            active_border = {
+                colors = {
+                    "rgb(f5c2e7)",
+                    "rgb(000000)",
+                },
+                angle = 45,
+            },
+            inactive_border = {
+                colors = {
+                    "rgb(242731)",
+                },
+            },
         },
         resize_on_border = true,
+        extend_border_grab_area = 30,
+        hover_icon_on_border = true,
+        resize_corner = 5,
         allow_tearing = false,
         layout = "dwindle",
     },
-
+    scrolling = {
+        focus_fit_method = 1,
+        explicit_column_widths = "0.5,1",
+        direction = "left",
+    },
     dwindle = {
         preserve_split = true,
     },
-
     master = {
-        new_status = "master",
+        new_status = inherit,
+        new_on_top = true,
+        new_on_active = "true",
+        smart_resizing = true,
     }
 })
